f2d01a4a07aa64d4b63fd28dfe0f5ed3dc18cda2
nemanja97 <ngacic30@gmail.com>
2018-10-31 23:45:19 +0100

    Initial commit

---
+++ eBook/src/main/java/com/nemanja97/eBook/EBookApplication.java
@@ -0,0 +1,12 @@
+package com.nemanja97.eBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookApplication.class, args);
+	}
+}
---
+++ eBook/src/test/java/com/nemanja97/eBook/EBookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nemanja97.eBook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EBookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
