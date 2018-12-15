999bf7667a721a1e8003c88665a6ecc11a22a536
Nemanja <ngacic30@gmail.com>
2018-12-15 22:19:37 +0100

    Some changes 2

--- eBook-Frontend/src/app/app-routing.module.ts
+++ eBook-Frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BookComponent } from './book/book.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: 'book',
     component: BookComponent
+  },
+  {
+    path: 'add-book',
+    component: AddBookComponent
   }
 ];
 
